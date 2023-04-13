3ad44770bfffb08ed6718c27f008e41a92735e52
kalab Afera <kalabafex@gmail.com>
2023-04-12 19:12:31 -0500

    fix

--- JavaScriptAssignment5/Assignment5Whole.js
+++ JavaScriptAssignment5/Assignment5Whole.js
@@ -123,3 +123,40 @@ let getNewArray = (args) => {
   return newString;
 };
 console.log(getNewArray(["relative", "real", "rolling"]));
+
+x = 1;
+var a = 5;
+var b = 10;
+var c = (a, b, c) => {
+  console.log(x);
+  console.log(a);
+  var f = (a, b, c) => {
+    b = a;
+    console.log(b);
+    b = c;
+    var x = 5;
+  };
+  f(a, b, c);
+  console.log(b);
+  var x = 10;
+};
+c(8, 9, 10);
+console.log(b);
+console.log(x);
+
+var x = 9;
+function myFunction() {
+  return x * x;
+}
+console.log(myFunction());
+x = 5;
+console.log(myFunction);
+
+var foo = 1;
+let bar = () => {
+  if (!foo) {
+    var foo = 10;
+  }
+  alert(foo);
+};
+bar();
